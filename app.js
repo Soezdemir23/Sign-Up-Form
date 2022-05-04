a9d08fe1071f7cc8e2fd423848d5ded4851e7b3d
Soezdemir23 <soezdemir3423@gmail.com>
2022-05-04 18:06:42 +0200

    Finish logic for layout, finish markup, finish css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let footerEl = document.querySelector('footer');
 let leftSideEl = document.querySelector('#left-side')
 
+let titleEl = document.querySelector('header')
+
 function getScrollHeight() {
     return document.documentElement.scrollHeight
 }
@@ -8,13 +10,19 @@ function getScrollHeight() {
 function getElemWidth() {
     return leftSideEl.offsetWidth
 }
-
+/**
+ * this is going to responsively adjust the titleby 33% and footer by 50% of the whole html being displayed. 
+ * this way we can make sure the User is always getting greeted by the same ol' design 
+ */
 function applyChange() {
     footerEl.style.top = `${getScrollHeight()-50}px`;
+    titleEl.style.top = `${getScrollHeight()/3}px`
     if (getElemWidth() < 200) footerEl.style.display = "none"
     else {
         footerEl.style.left = `${(getElemWidth()/2-footerEl.offsetWidth/2)}px`
-    }   
+        
+    }
+
 }
 //we need to set up the current footer position
 applyChange()
